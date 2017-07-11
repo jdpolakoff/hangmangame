01c012f7e3ae528c0c6f56eb3fb3f12dc168775f
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-11 17:03:22 -0400

    linked input to console

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//console.log('im linked')
+console.log('im linked')
+
+var input = $('#enterword')
+
+console.log(input.val())
+
+//create an event listener that takes in input and also generates empty divs for set number of letters
+button.on
