3196fdb3c3bb00fe515d55363c36d1250ff043c4
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-12 09:53:58 -0400

    created letter bank

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ console.log('im linked')
 
 var input = $('input')
 var button = $('button')
+var letterBoard = $('.wordgoeshere')
 
 //create function to take input and print it
 var handleInput = function (e){
   e.preventDefault()
   console.log(input.val())
+  input.val().length
+  console.log(input.val().length)
+  for (i = 0; i < input.val().length; i++) {
+    letterBoard.append('_')
+      letterBoard.css('font-family', 'Inconsolata', 'monospace')
+      letterBoard.css('font-size', '56px')
+      letterBoard.css('text-align', 'center')
+  }
 }
 
 //event listener for input to be printed when button pressed
 button.on('click', handleInput)
 
 //create function that counts number of letters in input and creates gameboard
+var countLetters = function() {
+  input.val().length
+  console.log(input.val().length)
+}
+
+countLetters()
+
 
 
 //create function that compares clicked letters to input
